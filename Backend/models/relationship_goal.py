c2c8760aecb1422b85d0c37c2c4de47b9edfd0df
JulRiv07 <Julian.rivera@utp.edu.co>
2026-02-19 16:07:58 -0500

    Limpieza de main - rama solo para integración

--- Backend/models/relationship_goal.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Relationship_goal(BaseModel):
-    id_relationship_goal: Optional[str] = None
-    code_name: str
-    display_name_es: str
-    description_es: str
--- Backend/models/therian_type.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Therian_type(BaseModel):
-    id_therian_type: Optional[str] = None
-    code_name: str
-    display_name_es: str
-    description_es: str
