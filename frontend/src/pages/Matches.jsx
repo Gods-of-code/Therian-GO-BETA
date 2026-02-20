7dcdcb85ca7951092af08c3573dcbfceb35a7f62
Martín <martin.barrero@utp.edu.co>
2026-02-20 00:29:57 -0600

    nada

--- frontend/src/pages/Matches.jsx
+++ frontend/src/pages/Matches.jsx
@@ -2,6 +2,7 @@ import Button from "../components/Button";
 
 export default function Matches() {
     return (
+        
         <div className="page">
         <h1>Matches</h1>
         <p>Aquí aparecerán los matches.</p>
